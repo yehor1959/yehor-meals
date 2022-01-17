49a532b27b4258b4c706ae675fb158567e7e12da
Yehor <zaitsev.yehor@gmail.com>
2022-01-17 16:06:57 +0100

    first commit

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,16 @@
+import { Fragment } from "react/cjs/react.production.min";
+
+import AvailableMeals from "./AvailableMeals";
+import MealsSummary from "./MealsSummary";
+
+const Meals = () => {
+    return (
+        <Fragment>
+            <MealsSummary />
+            <AvailableMeals />
+        </Fragment>
+    )
+};
+
+export default Meals;
+
